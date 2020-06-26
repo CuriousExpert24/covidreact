86640c59b8791e8778d09820701c721ce77a9e7d
CuriousExpert24 <karthikbaga04@gmail.com>
2020-06-25 22:39:18 -0500

    Updates

---
+++ precache-manifest.75067a03220d1f40933fb9d4ad568d5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5cd56dd977303cc72a2075d9364c8ba",
+    "url": "/covidreact/index.html"
+  },
+  {
+    "revision": "15c7ece3f858087cc389",
+    "url": "/covidreact/static/css/main.db2ea88c.chunk.css"
+  },
+  {
+    "revision": "ceb140fecc27bdf6ae33",
+    "url": "/covidreact/static/js/2.f6214fe1.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidreact/static/js/2.f6214fe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15c7ece3f858087cc389",
+    "url": "/covidreact/static/js/main.da75ddc1.chunk.js"
+  },
+  {
+    "revision": "dc13025821d532025842",
+    "url": "/covidreact/static/js/runtime-main.c7097bd2.js"
+  },
+  {
+    "revision": "708767846653d040bac4d1446fe531d4",
+    "url": "/covidreact/static/media/covid.70876784.png"
+  }
+]);
